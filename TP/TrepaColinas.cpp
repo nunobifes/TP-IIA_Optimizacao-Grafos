be29d85f6657bd192b07c808279e426d1a57c3e7
Nuno Rocha <intersect.nerd@gmail.com>
2018-12-21 02:02:51 +0000

    Added a whole lot and remove another big lot, Trepa-colinas is working!!!

--- TP/TrepaColinas.cpp
+++ TP/TrepaColinas.cpp
@@ -1,64 +1,54 @@
 ﻿#include "TrepaColinas.h"
 
 
-TrepaColinas::TrepaColinas(const Grafo& grafo, int vert, int iter): grafo(grafo), vert(vert), n_iter(iter)
+TrepaColinas::TrepaColinas(const Grafo& grafo, int iter): grafo(grafo), vert(grafo.get_verts()), n_iter(iter)
 {
 	custo = 0;
 	mbf = 0.0;
 	best_custo = 0;
-	sol = gera_sol_inicial();
+	
+	Funcoes::inicializector(&best, vert);
 }
 
 vector<int> TrepaColinas::gera_sol_inicial()
 {
-	int x;
+	
 	vector<int> sol;
 	for (int i = 0; i < vert; ++i)
-		sol.push_back(0);
-	for (int i = 0; i < vert/2; ++i)
-	{
-		do
-			x = Funcoes::random_l_h(0, vert - 1);
-		while (sol[x] != 0);
-		sol[x] = 1;
-	}
+		sol.push_back(Funcoes::bynarit());
+	
 	return sol;
 }
 
 void TrepaColinas::escreve_sol(vector<int> sol, int vert)
 {
-	cout << "\nConjunto A: ";
-	for (auto i = 0; i < vert; i++)
-		if (sol[i] == 0)
-			cout << i << " ";
-	cout << endl << endl << "Conjunto B: ";
+	cout << "\nSet de Vertices Independentes: ";
 	for (auto i = 0; i < vert; i++)
 		if (sol[i] == 1)
-			cout << i << " ";
+			cout << i+1 << " ";
+	
 	cout << "\n";
 }
 
 void TrepaColinas::substitui(vector<int> *sol1, vector<int> *sol2, int vert)
 {
-	if (sol1->empty())
-		for (auto i = 0; i < vert; ++i)
-			sol1->push_back(sol2->at(i));
-	else
-		for (auto i = 0; i < vert; ++i)
+	for (int i = 0; i < vert; ++i)
 			sol1->at(i) = sol2->at(i);
 }
 
 int TrepaColinas::trepa_colinas(vector<int> *sol, Grafo g, int vert, int num_iter)
 {
 	vector<int> nova_sol;
+	Funcoes::inicializector(&nova_sol, vert);
 	int custo, custo_viz;
 
 	custo = calcula_fit(*sol, g.get_matriz(), vert);
 	for (auto i = 0; i < num_iter; i++)
 	{
 		gera_vizinho(sol, &nova_sol, vert);
+		//gera_vizinho2(sol, &nova_sol, vert);
 		custo_viz = calcula_fit(nova_sol, g.get_matriz(), vert);
-		if(custo_viz < custo)
+		if(custo_viz > custo)
 		{
 			substitui(sol, &nova_sol, vert);
 			custo = custo_viz;
@@ -70,11 +60,11 @@ int TrepaColinas::trepa_colinas(vector<int> *sol, Grafo g, int vert, int num_ite
 
 int TrepaColinas::calcula_fit(vector<int> sol, vector<vector<int>> mat, int vert)
 {
-	int total = 0;
+	int total = 0, conta = 0;
 
 	for (auto i = 0; i < vert; i++)
 	{
-		if(sol[i] == 0)
+		if(sol[i] == 1)
 		{
 			for (int j = 0; j < vert; ++j)
 				if (sol[j] == 1 && mat[i][j] == 1)
@@ -82,46 +72,78 @@ int TrepaColinas::calcula_fit(vector<int> sol, vector<vector<int>> mat, int vert
 			
 		}
 	}
-	return total;
+
+	if(total == 0)
+	{
+		for(int i = 0; i < vert; i++)
+		{
+			if (sol[i] == 1)
+				conta++;
+		}
+		return conta;
+	}
+	else
+	{
+		return -total;
+	}
 }
 
 void TrepaColinas::gera_vizinho(vector<int> *sol1, vector<int> *sol2, int n)
 {
+	int p1;
+	
+	for (int i = 0; i < n; ++i)
+		sol2->at(i) = sol1->at(i);
+		
+	
+	p1 = Funcoes::random_l_h(0, n - 1);
+	
+	sol2->at(p1) = !sol2->at(p1);
+	
+}
+
+void TrepaColinas::gera_vizinho2(vector<int> *sol1, vector<int> *sol2, int n)
+{
 	int p1, p2;
 
-	for (int i = 0; i < n; ++i)
-		sol2->push_back(sol1->at(i));
+
+		for (int i = 0; i < n; ++i)
+			sol2->at(i) = sol1->at(i);
+
+
+	p1 = Funcoes::random_l_h(0, n - 1);
+
+	sol2->at(p1) = !sol2->at(p1);
 
 	do
-		p1 = Funcoes::random_l_h(0, n - 1);
-	while (sol2->at(p1) != 0);
-	do
-			p2 = Funcoes::random_l_h(0, n - 1);
-	while (sol2->at(p2) != 0);
-	
-	sol2->at(p1) = 1;
-	sol2->at(p2) = 0;
+		p2 = Funcoes::random_l_h(0, n - 1);
+	while (p2 == p1);
+
+	sol2->at(p2) = !sol2->at(p2);
+
 }
+
 
 
 void TrepaColinas::run()
 {
 	int k;
-	for(k = 0; k < 10; k++){
+	for(k = 0; k < 20; k++){
+		sol = gera_sol_inicial();
 		escreve_sol(sol, vert);
 		custo = trepa_colinas(&sol, grafo, vert, n_iter);
 		cout << "\nRepeticao: " << k;
 		escreve_sol(sol, vert);
-		cout << "Custo Final" << custo << endl;
+		cout << "Custo Final " << custo << endl;
 		mbf += custo;
-		if(k == 0 || best_custo > custo)
+		if(k == 0 || best_custo < custo)
 		{
 			best_custo = custo;
 			substitui(&best, &sol, vert);
 		}
 	}
-	cout << "\n\nMBF: " << mbf / k << endl;
-	cout << "\nMelhor Solucao Encontrada: ";
+	cout << "\n\nMBF: " << setprecision(3) << mbf / k << endl;
+	cout << "\nConjunto Maximo de Vertices Independentes: ";
 	escreve_sol(best, vert);
 	cout << "Custo final: " << best_custo << endl;
 
--- TP/TrepaColinas.h
+++ TP/TrepaColinas.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "Funcoes.h"
 #include "Grafo.h"
 
@@ -11,13 +12,14 @@ class TrepaColinas
 {
 public:
 
-	TrepaColinas(const Grafo& grafo, int vert, int iter);
+	TrepaColinas(const Grafo& grafo, int iter);
 	vector<int> gera_sol_inicial();
 	void escreve_sol(vector<int> sol, int vert);
 	void substitui(vector<int> *sol1, vector<int> *sol2, int vert);
 	int trepa_colinas(vector<int> *sol, Grafo g, int vert, int num_iter);
 	int calcula_fit(vector<int> sol, vector< vector<int> > mat, int vert);
 	void gera_vizinho(vector<int> *sol1, vector<int> *sol2, int n);
+	void gera_vizinho2(vector<int>* sol1, vector<int>* sol2, int n);
 	void run();
 
 private:
